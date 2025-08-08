13e70265bb3edc801459a5229f17afad23772cba
Abhijeet Singh <158417658+AbhijeetSingh12345@users.noreply.github.com>
2025-08-08 17:39:35 +0530

    Delete weather-app directory

--- weather-app/src/components/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import '../styles/ErrorMessage.css';
-
-export default function ErrorMessage({ message }) {
-  return (
-    <div className="error-container">
-      <p className="error-message">{message}</p>
-    </div>
-  );
-}
